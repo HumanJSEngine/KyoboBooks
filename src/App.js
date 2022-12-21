c454104476c7d7a98ccb0d4cbdd7b8ed5ab957cd
Yenhj <yen.greact@gmail.com>
2022-12-21 15:19:45 +0900

    paging 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /** @format */
 
-import { Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 // axios API
 import instance from "./api/axios";
 import requests from "./api/request";
@@ -14,37 +14,24 @@ import "./App.css";
 
 const App = () => {
   const [bookList, setBookList] = useState([]);
-  const [count, setCount] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage] = useState(8);
+  // const [bookDetail, setBookDetail] = useState([]);
 
-  const [indexOfLastPost, setindexOfLastPost] = useState(0);
-  const [indexOfFirstPost, setindexOfFirstPost] = useState(0);
-  const [currentPost, setcurrentPost] = useState(0);
-  const [bookDetail, setBookDetail] = useState([]);
-  
   const fetchData = async () => {
     const resultBookList = await instance.get(requests.bookList);
     setBookList(resultBookList.data.list);
-    const resultBookDetail = await instance.get(requests.bookDetail);
-    setBookDetail(resultBookDetail.data.list);
+    // const resultBookDetail = await instance.get(requests.bookDetail);
+    // setBookDetail(resultBookDetail.data.list);
   };
 
   useEffect(() => {
     fetchData();
-    setindexOfLastPost(currentPage * postPerPage);
-    setIndexOfFirstPost(indexOfLastPost - postPerPage);
-    setCurrentPosts(items.slice(indexOfFirstPost, indexOfLastPost));
-  }, [currentPage, indexOfFirstPost, indexOfLastPost, items, postPerPage], []);
+  }, []);
 
-  const setPage = (e) => {
-    setCurrentPage(e);
-  };
   return (
     <div className="container">
       <Header />
       <Routes>
-        <Route path="/" element={<BookList bookList={bookDetail} />} />
+        <Route path="/" element={<BookList BookList={bookList} />} />
         <Route
           path="/bookmain/:id"
           element={<BookMain BookMain={bookList} />}
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,69 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import instance from "./../api/axios";
+import requests from "./../api/request";
 import { Link } from "react-router-dom";
+import Pagination from "react-js-pagination";
 import styled from "styled-components";
-import Paging from "./Paging";
 
 const BookList = (props) => {
-  const list = props.bookList.map((item, index) => {
-    return (
-      <Link to={`/bookMain/${item.biSeq}`} key={item.biSeq}>
-        <div className="col txt-left mb-5">
-          <div className="card h-100">
-            <img src={item.photo} className="card-img-top" alt={item.biTitle} />
-            <div className="card-body bg-wh">
-              <div className="card-title mt-2">
-                <b className="bg-wh">{item.biTitle}</b>
-              </div>
-              <p className="fs-6 bg-wh">
-                {item.biSubTitle} · {item.biAuthor}
-              </p>
-              <p className="fs-5 bg-wh">
-                <span className="badge text-bg-success m-2">10%</span>
-                {item.biPrice}원
-              </p>
-            </div>
-          </div>
-        </div>
-      </Link>
-    );
-  });
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState(8);
+
+  const fetchData = async () => {
+    const resultData = await instance.get(requests.bookList);
+    setData(resultData.data.list);
+  };
+  const getClick = () => {
+    axios
+      .get("http://192.168.0.111:9988/api/listall/book")
+      .then((res) => setData(res.data));
+  };
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
+  const itemChange = (e) => {
+    setItems(Number(e.target.value));
+  };
+
+  useEffect(() => {
+    fetchData();
+    return () => {};
+  }, []);
 
   return (
     <div>
-      <Paging />
-      {/* <Layout>
-        {/* 유저가 페이지 당 표시할 게시물 수 변경 o */}
-      {/* <label>
-          보기:&nbsp;
-          <select
-            type="number"
-            value={limit}
-            onChnage={({ target: { value } }) => setLimit(Number(value))}
-          >
-            <option value="8">8</option>
-            <option value="5">5</option>
-            <option value="2">2</option>
-          </select>
-        </label>
-      </Layout> */}
+      <h1 className="mt-4">Boooklsit</h1>
+      <Mg className="mb-2">
+        {getClick}
+        <select name="items" onChange={itemChange}>
+          <option value="4">4개씩 보기</option>
+          <option value="8">8개씩 보기</option>
+          <option value="12">12개 보기</option>
+        </select>
+      </Mg>
 
-      <main>
-        <h1 className="mt-4">Boooklsit</h1>
-        <div className="row row-cols-4 mt-4 container">{list}</div>
-      </main>
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={items}
+        totalItemsCount={data.length - 1}
+        pageRangeDisplayed={8}
+        onChange={handlePageChange}
+      ></Pagination>
+
+      <div className="row row-cols-4 mt-4container ">
+        {data
+          .slice(items * (page - 1), items * (page - 1) + items)
+          .map((item, i) => {
+            return (
+              <Link
+                to={`/bookMain/${item.biSeq}`}
+                key={item.biSeq}
+                style={{ textDecoration: "inherit", color: "inherit" }}
+              >
+                <div className="col txt-left mb-5">
+                  <div className="card h-100">
+                    <img
+                      src={`http://192.168.0.111:9988/api/image/${item.biIiUri}`}
+                      className="card-img-top"
+                      alt={item.biTitle}
+                      // style={{width: 100, height: 100}}
+                    />
+                    <div className="card-body bg-wh">
+                      <div className="card-title mt-2">
+                        <b className="bg-wh">{item.biTitle}</b>
+                      </div>
+                      <p className="fs-6 bg-wh">{item.biAuthor}</p>
+                      <p className="fs-6 bg-wh">{item.biPublisher}</p>
+                      <p className="fs-5 bg-wh">
+                        <span className="badge text-bg-success m-2">10%</span>
+                        {item.biPrice}원
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={items}
+        totalItemsCount={data.length - 1}
+        pageRangeDisplayed={8}
+        onChange={handlePageChange}
+      ></Pagination>
     </div>
   );
 };
 
-// const Mgzero = styled.mgzero`
-//   margin: 0;
-// `;
-// const Layout = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   max-width: 800px;
-//   margin: 0 auto;
-// `;
-
+const Mg = styled.div`
+  display: flex;
+  justify-content: right;
+  margin-bottom: 60px;
+`;
 export default BookList;
